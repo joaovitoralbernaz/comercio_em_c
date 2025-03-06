778a466571936a9f2d50d4f076397abc8f3921f0
Bia <159267437+anebeatriz@users.noreply.github.com>
2025-03-06 09:18:41 -0300

    Update comercio.c

--- comercio.c
+++ comercio.c
@@ -4,48 +4,60 @@ int main(){
     char escolha;
     char sair;
     char estoque;
-    printf("Digite e para estoque, v para venda, f para finaceiro, digite s para sair: ");
+    printf("Digite E para estoque, V para vendas, F para controle finaceiro e digite S para sair: ");
     scanf("%c" , &escolha);
      
-    
-    
+     
     switch( escolha ){
-        case 'e':
-            printf("Controle de estoque: \n");
-            printf("----------------------------------------------------------------------------------- \n");
-            printf(" Itens disponíveis: \n \n");
-            printf("   101 - produto A -- 10 un; \n");
-            printf("   102 - produto B -- 23 un; \n");
-            printf("   103 - produto C -- 31 un: \n \n");
-            
-            printf("Você deseja sair do programa? \n");
-            printf("Para sair digite s, para voltar digite v: ");
-            scanf(" %c" , &sair);
-            switch(sair){
-                case 's':
-                    printf("Programa encerrado \n");
-                    break;
-                    }
-            printf("   \n");
-            
+        case 'E':
+        printf("--------------------------------------------------------------------------------------------------- \n");
+        printf("Controle de estoque \n");
+        printf("     *Itens Disponíveis: \n \n");
+        printf("   101 - Produto A -- 10 unidades; \n");
+        printf("   102 - Produto B -- 23 unidades; \n");
+        printf("   103 - Produto C -- 31 unidades: \n \n");
+        printf("Voltar ao menu principal? \n");
+        scanf(" %c" , &sair);
+        switch(sair){
+            case 'S':
+            printf("Programa encerrado \n");
             break;
-            
-            
-        case 'v' :   
-            printf("Controle de venda \n");
+        }
+        
+        case 'F':
+        printf("--------------------------------------------------------------------------------------------------- \n");
+        printf("Controle financeiro \n");
+        printf("     *Opções: \n \n");
+        printf("201 - Registrar Despesas \n");
+        printf("202 - Registrar Receitas \n");
+        printf("203 - Consultar Saldo \n \n");
+        printf("Voltar ao menu principal? \n");
+        scanf(" %c" , &sair);
+        switch(sair){
+            case 'S':
+            printf("Programa encerrado \n");
             break;
-        case 'f':
-            printf("Controle financeiro \n");
+        }
+        
+        case 'V' :
+        printf("--------------------------------------------------------------------------------------------------- \n");
+        printf("Vendas \n");
+        printf("     *Itens Disponíveis: \n \n");
+        printf("301 - Produto A -- R$22,00 \n");
+        printf("302 - Produto B -- R$40,00 \n");
+        printf("303 - Produto C -- R$53,00 \n \n");
+        printf("Voltar ao menu principal? \n");
+        scanf(" %c" , &sair);
+        switch(sair){
+            case 'S':
+            printf("Programa encerrado \n");
             break;
-        case 's':
-            printf("Programa encerrado");
-            break;
+        }
+       
         default:
-            printf("Opção invalida \n");
-            break;
+        printf("Opção Inválida \n");
+        break;
     }
-/* 
-*/
 
     return 0;
 }
